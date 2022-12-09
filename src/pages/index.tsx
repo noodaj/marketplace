4a48eae5a33b8be350b11955067bdddc31e88849
noodaj <jadon.loi.68@gmail.com>
2022-12-08 19:52:12 -0800

    base

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <main>test</main>
+    </>
+  );
+};
+
+export default Home;
